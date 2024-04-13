3ebbe4f0a140356e6f0977e869e97938e335e0c6
pgckpp <pgckpp@gmail.com>
2024-04-13 18:43:37 +0200

    qol

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 Element.offsetWidth - Element.clientWidth
 
-
 function toggleColorMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    
-    
 }
+
 function exitColorMode() {
     var element = document.body;
     element.classList.remove("dark-mode");
-    
-    
 }
-
-const toggleSwitch = document.querySelector('.switch');
 
 function switchTheme(e) {
     var logo = document.querySelector('.logo')
     logo.classList.toggle("invert");
-    
     if (e.target.checked) {
         toggleColorMode();
     }
@@ -28,7 +21,19 @@ function switchTheme(e) {
     }
 }
 
+const toggleSwitch = document.querySelector('.switch');
+toggleSwitch.addEventListener("input", (e) => switchTheme(e));
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+
+const fontSizeSlider = document.getElementById("textslider");
+const displayText = document.getElementById("textsize");
+
+fontSizeSlider.addEventListener("input", () => {
+    const fontSize = fontSizeSlider.value;
+    displayText.style.fontSize = `${fontSize}px`;
+});
 
 
